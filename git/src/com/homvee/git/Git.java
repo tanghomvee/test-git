d9c6e4d81afdf6dca640f103a79c27febc0905c0
tanghongwei <Wert324tang>
2018-02-09 16:30:46 +0800

    master feature-400000

--- git/src/com/homvee/git/Git.java
+++ git/src/com/homvee/git/Git.java
@@ -14,6 +14,7 @@ public class Git {
         System.out.println("master feature-100000");
         System.out.println("master feature-200000");
         System.out.println("master feature-300000");
+        System.out.println("master feature-400000");
 
         System.out.println("feature-1 commit-2");
         System.out.println("feature-1 commit-1");
